6dd2d0d3503dca71ad4e296332d3362565a3676c
horikawakeita <keita.horikawa@rakus.co.jp>
2021-06-08 11:31:18 +0900

    404,500各エラーページとエラー遷移を行うハンドラの作成

---
+++ src/main/java/jp/co/sample/emp_management/common/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package jp.co.sample.emp_management.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * アプリケーション内で処理されなかった例外をここでキャッチし、
+ * エラーページへ遷移.
+ * 
+ * @author horikawakeita
+ *
+ */
+@Component
+public class GlobalExceptionHandler implements HandlerExceptionResolver {
+
+	private static final Logger LOGGER
+			= LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+	@Override
+	public ModelAndView resolveException(
+			HttpServletRequest request, 
+			HttpServletResponse response, 
+			Object obj, 
+			Exception e) {
+		LOGGER.error("システムエラーが発生しました！", e);
+		return null;
+	}
+}
